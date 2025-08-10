adfb4e0ee760c55a0b5e060d16d9c7b93d9a5446
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2025-08-10 16:53:38 -0400

    add basic content

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,16 +18,14 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-2 hover:underline"
+            className="flex hover:underline"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-3">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[200px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
+            <div className="flex-1">
+              <h3>{post.metadata.title}</h3>
+            </div>
+            <div className="flex flex-col items-end">
+              <p className="text-sm text-gray-500">{post.metadata.publishedAt}</p>
             </div>
           </Link>
         ))}
