71e91f03caa264cb6c1989d47e13d0f1a347f10d
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2025-08-16 16:00:22 -0400

    fix for IOS26 Safari viewport

--- app/components/sky-gradient.tsx
+++ app/components/sky-gradient.tsx
@@ -56,10 +56,6 @@ export default function SkyGradient({
       style={{
         background: gradient,
         position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
         zIndex: -1,
         pointerEvents: 'none'
       }}
